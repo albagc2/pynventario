3605814db4de9a700eabed047f42b10f42ae7d72
Alba GC <albagoncebrian@gmail.com>
2024-11-11 07:43:51 +0100

    included setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+APP = ['code/gui.py']  # Cambia a tu archivo principal
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['pillow', 'google.cloud.vision', 'openpyxl'],  # Solo los módulos necesarios
+    'includes': ['tkinter']  # Incluye tkinter si usas una GUI
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
